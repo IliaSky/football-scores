4cb252d58adc9863705ee1f684a453ae9961b297
IliaSky <iliaskywalker@gmail.com>
2018-08-09 19:24:15 +0300

    Add mongodb database.

--- server/db.js
+++ server/db.js
@@ -1,11 +1,34 @@
 const bcrypt = require('bcrypt');
+const MongoClient = require('mongodb').MongoClient;
 
-const dummyUsers = ['A', 'B', 'C'].map(
-  name => ({username: name, password: bcrypt.hashSync(name, 10)})
-);
-const findUser = (username, cb) => {
-  const user = dummyUsers.find(user => user.username == username);
-  cb(null, user);
+const url = 'mongodb://localhost:27017/';
+
+const api = {
+  findUser(username, cb) {
+    this.db.collection("users").find({username}, cb);
+  },
+  addUser({username, password}, cb) {
+    bcrypt.hash(password, 10, (err, hash) => {
+      this.db.collection("users").insert({username, password: hash}, cb);
+    });
+  }
 };
 
-module.exports = {findUser};
+MongoClient.connect(url, {useNewUrlParser: true}, (err, db) => {
+  if (err) {
+    console.log(err);
+    process.exit();
+  }
+  api.db = db.db('football-scores');
+  console.log('connected to mongo server');
+});
+
+// const dummyUsers = ['A', 'B', 'C'].map(
+//   name => ({username: name, password: bcrypt.hashSync(name, 10)})
+// );
+// const findUser = (username, cb) => {
+//   const user = dummyUsers.find(user => user.username == username);
+//   cb(null, user);
+// };
+
+module.exports = api;
